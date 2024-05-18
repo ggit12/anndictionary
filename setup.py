1611bbd56a46e68af602355f626e98251730edc3
ggit12 <115842122+ggit12@users.noreply.github.com>
2024-05-18 15:54:54 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adict',
+    version='0.1',
+    packages=find_packages(),
+    description='Conveniently process a dictionary of anndatas (adata_dict)',
+    author='ggit12',
+    # author_email='your.email@example.com',
+    license='BSD-3-Clause',
+    install_requires=[
+        # list of packages this package depends on
+        'numpy', 
+        'pandas',
+        'sklearn',
+        'scanpy',
+        'anndata',
+        'os',
+        'random',
+        'itertools',
+        'IPython',
+        'scipy',
+        'seaborn',
+        'matplotlib'
+    ],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.org/classifiers/
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
